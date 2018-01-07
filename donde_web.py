d25abddb9a1f9ed041d0403a5799bc4295f32a48
K H <khendarg@ucsd.edu>
2018-01-07 00:49:20 -0800

    update

--- donde_web.py
+++ donde_web.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-print('Content-type: text/html\r\n\r\n')
 from __future__ import print_function
+print('Content-type: text/html\r\n\r\n')
 
-import cgi, cgitb
+import cgi
+import cgitb
+import donde
+import os, sys, re
 
-
-import Deuterocol1, donde
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+import Deuterocol1
 
 def get_queries(fams, p1dir='deuterocol1', pdbtmdir='pdbtm'):
 	p1 = Deuterocol1.Protocol1(pdbtmdir=pdbtmdir, outdir=p1dir, offline=True)
@@ -32,5 +36,41 @@ def main(fams, p1dir='deuterocol1', pdbtmdir='pdbtm', max_distance=5., min_dista
 #main(fams=['1.H.1','8.A.16'])
 
 if __name__ == '__main__':
-	#form = cgi.FieldStorage()
-	print('TEST')
+	form = cgi.FieldStorage()
+
+	fams = form.getvalue('families')
+	if fams is None: 
+		print('[ERROR]: Please specify at least one TCID<br/><a href="javascript:history.back()">Return to form</a><br/>')
+		exit()
+	elif not fams: 
+		print('[ERROR]: Please specify at least one TCID<br/><a href="javascript:history.back()">Return to form</a><br/>')
+		exit()
+	else: fams = re.split('\s*,\s*', fams)
+
+	maxdist = form.getvalue('maxdistance')
+	if maxdist is None: maxdist = 5.0
+	elif not maxdist: maxdist = 5.0
+	else: 
+		try: maxdist = float(maxdist)
+		except ValueError: maxdist = None
+
+	mindist = form.getvalue('mindistance')
+	if mindist is None: mindist = None
+	elif not mindist.strip(): mindist = None
+	else:
+		try: mindist = float(mindist)
+		except ValueError: mindist = None
+
+	include = form.getvalue('include')
+	if include is None: include = None
+	elif not include.strip(): include = None
+	else: include = re.split('\s*,\s*', include)
+
+	exclude = form.getvalue('exclude')
+	if exclude is None: exclude = None
+	elif not exclude.strip(): exclude = None
+	else: exclude = re.split('\s*,\s*', exclude)
+
+	print('<pre>')
+	main(fams=fams, max_distance=maxdist, min_distance=mindist, include=include, exclude=exclude)
+	print('</pre>')
